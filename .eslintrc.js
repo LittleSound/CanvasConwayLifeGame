0690781de2e72b79a3322ea200d66bfc012381a0
LittleSound <464388324@qq.com>
2021-06-24 15:05:51 +0800

    修改 ESLint 的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,29 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
+    'plugin:vue/strongly-recommended',
+    '@vue/standard',
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+    'vue/singleline-html-element-content-newline': ['error', {
+      ignores: ['p', 'span', 'time', 'a']
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false
+      }
+    }]
+  }
+}
